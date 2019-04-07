db1c76849b9c875ec9fce6f38750beb11c8ebb1d
Carlos A Beltran <carlosbeltran90@gmail.com>
2019-04-07 01:36:09 -0400

    completed section3 pill info

--- js/logicQuery.js
+++ js/logicQuery.js
@@ -96,39 +96,133 @@ function sendRequestToTranslate(){
 		"format": "text"
 	};
 
-	var api_url = 'https://translation.googleapis.com/language/translate/v2?key='
-	var key = 'AIzaSyB7kj2whOaO6ykBKEZBLV_IlHOGOsEsbns'
-	$.ajax({
-		type: "POST",
-		url: `${api_url}${key}`,
-		contentType: 'text/plain',
-		data: JSON.stringify(request),
-		success: function (response) {
-      // console.log(response.data.translations[0].translatedText);
-      showTranslatedText.innerHTML = response.data.translations[0].translatedText;
-    },
-    error: function (data, textStatus, errorThrown) {
-    	console.log('error: ' + errorThrown);
-    }
-  });  
+	if(inputText != ""){
+		var api_url = 'https://translation.googleapis.com/language/translate/v2?key='
+		var key = 'AIzaSyB7kj2whOaO6ykBKEZBLV_IlHOGOsEsbns'
+		$.ajax({
+			type: "POST",
+			url: `${api_url}${key}`,
+			contentType: 'text/plain',
+			data: JSON.stringify(request),
+			success: function (response) {
+	      // console.log(response.data.translations[0].translatedText);
+	      showTranslatedText.innerHTML = response.data.translations[0].translatedText;
+	    },
+	    error: function (data, textStatus, errorThrown) {
+	    	console.log('error: ' + errorThrown);
+	    }
+	  });  
+	}
+	else{
+		alert("There is nothing to translate")
+	}
 }
 
 //the logic below is to send pill numbers to the API and receibe the info
 function sendRequestToRxInfo(){
 	var pillNumber = document.getElementById("pill-num").value;
-	// var showTranslatedText = document.getElementById("translated-text");
+	var drugName = document.getElementById("drug-name").value;
 
-	var api_url = 'https://rximage.nlm.nih.gov/api/rximage/1/rxnav?imprint='
-	$.ajax({
-		type: "GET",
-		url: `${api_url}${pillNumber}`,
-		contentType: 'text/plain',
-		// data: JSON.stringify(request),
-		success: function (response) {
-      console.log(response);
-    },
-    error: function (data, textStatus, errorThrown) {
-    	console.log('error: ' + errorThrown);
-    }
-  });  
+	$('#info-result').empty();
+	
+	if(!(pillNumber == "") && !(drugName == "")){
+		var api_url = `https://rximage.nlm.nih.gov/api/rximage/1/rxnav?imprint=${pillNumber}&name=${drugName}`
+		$.ajax({
+			type: "GET",
+			url: api_url,
+			contentType: 'text/plain',
+			// data: JSON.stringify(request),
+			success: function (response) {
+	      const arr_length = response.nlmRxImages.length
+
+	      if(arr_length == 0){
+	      	alert("No results found, please enter a new pill number OR name.")
+	      }
+
+	      for(let i = 0; i < arr_length; i++){
+	      	$('#info-result').append(
+			      		`<div class="row info-box">
+			      		<div class="col-md" style="flex-grow: 0;">
+			      		<a href="${response.nlmRxImages[i].imageUrl}" target="_blank"><img id="pill-img" src="${response.nlmRxImages[i].imageUrl}"></a>
+			      		</div>
+			      		<div class="col">
+			      		<h3 style="padding-top: 25px;"><b>Name:</b> <span id="tc_name">${response.nlmRxImages[i].name}</span></h3>
+			      		<h3><b>Manufacture:</b> <span id="tc_email">${response.nlmRxImages[i].labeler}</span></h3>
+			      		
+			      		<h3><b>Rxcui #:</b> <span id="tc_phone">${response.nlmRxImages[i].rxcui}</span></h3>
+			      		</div>
+			      		</div>`)
+	      }
+	    },
+	    error: function (data, textStatus, errorThrown) {
+	    	console.log('error: ' + errorThrown);
+	    }
+	  }); 
+	}
+	else if(drugName == ""){
+		var api_url = 'https://rximage.nlm.nih.gov/api/rximage/1/rxnav?imprint='
+		$.ajax({
+			type: "GET",
+			url: `${api_url}${pillNumber}`,
+			contentType: 'text/plain',
+			// data: JSON.stringify(request),
+			success: function (response) {
+	      const arr_length = response.nlmRxImages.length
+
+	      if(arr_length == 0){
+	      	alert("No results found, please enter a new pill number OR name.")
+	      }
+
+	      for(let i = 0; i < arr_length; i++){
+	      	$('#info-result').append(
+			      		`<div class="row info-box">
+			      		<div class="col-md" style="flex-grow: 0;">
+			      		<a href="${response.nlmRxImages[i].imageUrl}" target="_blank"><img id="pill-img" src="${response.nlmRxImages[i].imageUrl}"></a>
+			      		</div>
+			      		<div class="col">
+			      		<h3 style="padding-top: 25px;"><b>Name:</b> <span id="tc_name">${response.nlmRxImages[i].name}</span></h3>
+			      		<h3><b>Manufacture:</b> <span id="tc_email">${response.nlmRxImages[i].labeler}</span></h3>
+			      		<h3><b>Rxcui #:</b> <span id="tc_phone">${response.nlmRxImages[i].rxcui}</span></h3>
+			      		</div>
+			      		</div>`)
+	      }
+	    },
+	    error: function (data, textStatus, errorThrown) {
+	    	console.log('error: ' + errorThrown);
+	    }
+	  }); 
+	}else{
+		var api_url = 'https://rximage.nlm.nih.gov/api/rximage/1/rxnav?name='
+		$.ajax({
+			type: "GET",
+			url: `${api_url}${drugName}`,
+			contentType: 'text/plain',
+			// data: JSON.stringify(request),
+			success: function (response) {
+	      // console.log(response.nlmRxImages.length);
+	      const arr_length = response.nlmRxImages.length
+
+	      if(arr_length == 0){
+	      	alert("No results found, please enter a new pill number OR name.")
+	      }
+
+	      for(let i = 0; i < arr_length; i++){
+	      	$('#info-result').append(
+			      		`<div class="row info-box">
+			      		<div class="col-md" style="flex-grow: 0;">
+			      		<a href="${response.nlmRxImages[i].imageUrl}" target="_blank"><img id="pill-img" src="${response.nlmRxImages[i].imageUrl}"></a>
+			      		</div>
+			      		<div class="col">
+			      		<h3><b>Name:</b> <span id="tc_name">${response.nlmRxImages[i].name}</span></h3>
+			      		<h3><b>Manufacture:</b> <span id="tc_email">${response.nlmRxImages[i].labeler}</span></h3>
+			      		<h3><b>Rxcui #:</b> <span id="tc_phone">${response.nlmRxImages[i].rxcui}</span></h3>
+			      		</div>
+			      		</div>`)
+	      }
+	    },
+	    error: function (data, textStatus, errorThrown) {
+	    	console.log('error: ' + errorThrown);
+	    }
+	  }); 
+	}
 }
